5746776440fc3d8cfda6026c5f26f6667aa1efe9
Muhammad Nur Amin <muhnuramin956@gmail.com>
2020-10-21 10:32:18 +0700

    add-event

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 // props=properti
 export function Search(props){
+    function ButtonClick(){
+        alert('Tombol di klik')
+    }
+    function inputOnChange(event){
+        console.log(event.target.value)
+    }
+
     return(
         <div>
             <label>{props.label}</label>
-            <input type="text" placeholder={props.placeholder}></input>
-            <button>Mulai Mencari</button>
+            <input onChange={inputOnChange} type="text" placeholder={props.placeholder}></input>
+            <button onClick={ButtonClick}
+            >Mulai Mencari</button>
         </div>
     );
 
